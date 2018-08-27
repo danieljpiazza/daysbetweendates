4b5267f2301d919abf277b7e6fd9b29a60a24e8e
djpiazza <41491439+djpiazza@users.noreply.github.com>
2018-08-26 22:14:37 -0400

    Refactor, change layout from pack to grid, change "Clear" button to "Reset".
    General refactoring. Now using grid layout instead of pack. "Clear" button changed to "Reset", now resets DateEntry objects to the current date in addition to clearing the output field.

--- days_between_dates.py
+++ days_between_dates.py
@@ -1,15 +1,14 @@
-from datetime import datetime
+from datetime import datetime, date
 
 import tkinter as tk
-from tkinter import ttk, Label, Entry
+from tkinter import ttk, Label, Entry, PhotoImage
 
 from tkcalendar import DateEntry
 
-# Tkinter window settings.
-root = tk.Tk()
-root.wm_title("Date Math")
-root.resizable(width=False, height=False)
-root.geometry("225x235+200+25")
+root = tk.Tk()                              # Base Tkinter window.
+root.wm_title("")                           # Window title.
+root.resizable(width=False, height=False)   # Disable window resize.
+root.geometry("+200+25")                    # Initial window position.
 
 # Perform date math on user input and return the difference in days.
 def date_math():
@@ -24,7 +23,7 @@ def date_math():
     # Store read-only difference.days value in another variable.
     final_difference = difference.days
 
-    # Add one day if the user checked "Include End Date In Calculation".
+    # Add one day if the user checked "Include End Date".
     global check_var
     if check_var.get() == 1:
         final_difference += 1
@@ -42,6 +41,14 @@ def date_math():
     output_entry.configure(state="readonly")
 
 def clear():
+    # Build current date object.
+    current_date = date(now.year, now.month, now.day)
+
+    # Set DateEntry objects to the current date.
+    cal1.set_date(current_date)  
+    cal2.set_date(current_date) 
+
+    # Clear the output_entry field.
     output_entry.configure(state="normal")
     output_entry.delete(0, "end")
     output_entry.configure(state="readonly")
@@ -50,33 +57,33 @@ def clear():
 now = datetime.now()
 
 # Calendar #1 GUI elements.
-ttk.Label(root, text="Choose Date #1").pack(pady=(10, 0))
+ttk.Label(root, text="First Date").grid(row=0, column=0, sticky="W", padx=5, pady=5)
 
 cal1 = DateEntry(root, selectmode="day", locale="en_US", year=now.year, month=now.month, day=now.day)
-cal1.pack()
+cal1.grid(row=0, column=1, sticky="E", padx=5, pady=5)
 
 # Calendar #2 GUI elements.
-ttk.Label(root, text="Choose Date #2").pack(pady=(10, 0))
+ttk.Label(root, text="Second Date").grid(row=1, column=0, sticky="W", padx=5, pady=5)
 
 cal2 = DateEntry(root, selectmode="day", locale="en_US", year=now.year, month=now.month, day=now.day)
-cal2.pack()
+cal2.grid(row=1, column=1, sticky="E", padx=5, pady=5)
 
 # Output GUI elements.
-ttk.Button(root, text="Display Difference", command=date_math).pack(pady=(10, 5))
+ttk.Button(root, text="Calculate", command=date_math).grid(row=2, column=0, sticky="W", padx=5, pady=5)
 
 output_entry = Entry(root, width=15, justify="center", state="readonly")
-output_entry.pack()
+output_entry.grid(row=2, column=1, sticky="E", padx=5, pady=5)
 
 # Clear button for output field.
-ttk.Button(root, text="Clear Output", command=clear).pack(pady=(11, 0))
+ttk.Button(root, text="Reset", command=clear).grid(row=3, column=0, sticky="W", padx=5, pady=5)
 
 # Checkbox to determine if date math should include end date in calculations.
-check_var = tk.IntVar()
 # Lambda function, called on checkbox status change, calls date_math() with updated logic.
 # Only calls date_math() on status change if there is a value in the output_entry field.
-check_end_date = tk.Checkbutton(root, text="Include End Date in Calculation",
+check_var = tk.IntVar()
+check_end_date = tk.Checkbutton(root, text="Include End Date",
     variable=check_var, command=lambda: output_entry.get() and date_math())
-check_end_date.pack(pady=(10, 0))
+check_end_date.grid(row=3, column=1, padx=5, pady=5)
 
 # Call the main loop.
 root.mainloop()
